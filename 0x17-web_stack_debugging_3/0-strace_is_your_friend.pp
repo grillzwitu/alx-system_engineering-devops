15061b3abf2662cdae1ebccb5ab4738c83878fcd
grillzwitu <grillzwitu@gmail.com>
2021-09-29 18:48:19 +0000

    add full path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'fix typo':
   onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
